def4baf356efe82760e34db10b6428487dc61cb7
Hyeonju-Park <guswn3618@gmail.com>
2022-10-01 00:31:31 +0900

    carpet

--- Coding_Test/src/BFS/Carpet_0928.java
+++ Coding_Test/src/BFS/Carpet_0928.java
@@ -23,13 +23,31 @@ brown	yellow	return
 
 package BFS;
 
-public class BFS_0928 {
+import java.util.Iterator;
+
+public class Carpet_0928 {
 
 }
 class Solution {
-    public int[] solution(int brown, int yellow) {
-    	
-        int[] answer = {};
+	public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum = brown + yellow;
+        
+        for (int i = 3; i < sum; i++) {
+            int j = sum / i;
+            
+            if (sum % i == 0 && j >= 3) {
+                int col = Math.max(i, j);
+                int row = Math.min(i, j);
+                int center = (col - 2) * (row - 2);
+                
+                if (center == yellow) {
+                    answer[0] = col;
+                    answer[1] = row;
+                    return answer;
+                }
+            }
+        }
         return answer;
     }
 }
